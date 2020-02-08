6e1258790700c80cb17966222480784021b21b53
smith <smith@localhost.localdomain>
2020-02-07 23:56:00 -0500

    listening changeEdge

--- packages/shell/Store/sagaRoot.ts
+++ packages/shell/Store/sagaRoot.ts
@@ -1,5 +1,6 @@
 import { takeLatest } from '../../shared/saga/effects';
 import { Saga } from '../../shared/saga/private/types';
+import { changeEdge } from './actions';
 import { edge } from './sagas/edge';
 import { slide } from './sagas/slide';
 import { updateTaskCount1 } from './sagas/updateTaskCount1';
@@ -7,5 +8,5 @@ import { updateTaskCount1 } from './sagas/updateTaskCount1';
 export function* sagaRoot(): Saga {
   yield takeLatest('updateTaskCount1', updateTaskCount1);
   yield takeLatest('slide', slide);
-  yield takeLatest('changeEdge', edge);
+  yield takeLatest(changeEdge, edge);
 }
