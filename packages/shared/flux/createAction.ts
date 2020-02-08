58c2f77b1dbfe56d38d57df8bc0ef7fa91a698ba
smith <smith@localhost.localdomain>
2020-02-07 23:45:11 -0500

    action creator helper improvements

--- packages/shared/flux/createAction.ts
+++ packages/shared/flux/createAction.ts
@@ -1,37 +1,67 @@
 import { Action } from './flux';
 
-export type PrepareAction<P, M> =
-  | ((payload: P, meta: M) => { payload: P })
-  | ((payload: P, meta: M) => { meta: M })
-  | ((payload: P, meta: M) => { payload: P; meta: M });
+export type PrepareAction<P, M> = (
+  payload?: P,
+  meta?: M
+) => { payload: P } | { meta: M } | { payload: P; meta: M };
 
 type ActionType = { type: string };
 
-export type ActionCreator<P = void, M = void> = ActionType &
+export type AnyActionCreator = ActionType &
+  ((payload?: any, meta?: any) => { type: string; payload?: any; meta?: any });
+
+export type ActionCreator<P = any, M = any> = ActionType &
   ((payload: P, meta: M) => Action<P, M>);
 
+export type ActionCreatorDefaultPayload<P = void> = ActionType &
+  ((payload?: P) => Action<P, void>);
+
+export type ActionCreatorPrepared<
+  PA extends PrepareAction<any, any>
+> = ActionType & PA;
+
 export function createAction<P = void, M = void, T extends string = string>(
-  type: T,
-  prepareAction?: PrepareAction<P, M>
+  type: T
 ): ActionCreator<P, M>;
 
-export function createAction(type: string, prepareAction?: Function): any {
+export function createAction<
+  PA extends PrepareAction<any, any>,
+  T extends string = string
+>(type: T, prepareAction: PA): ActionCreatorPrepared<PA>;
+
+export function createAction<P = void, T extends string = string>(
+  type: T,
+  defaultPayload: P
+): ActionCreatorDefaultPayload<P>;
+
+export function createAction(type: string, prepareAction?: any): any {
+  let actionCreator: any;
   if (prepareAction) {
-    const actionCreator: any = (payload: any, meta: any) => {
+    if (typeof prepareAction === 'function') {
+      actionCreator = (payload: any, meta: any) => {
+        return {
+          type,
+          ...prepareAction(payload, meta),
+        };
+      };
+    } else {
+      actionCreator = (payload: any) => {
+        return {
+          type,
+          payload: payload ?? prepareAction,
+        };
+      };
+    }
+  } else {
+    actionCreator = (payload: any, meta: any) => {
       return {
         type,
-        ...prepareAction(payload, meta),
+        ...(payload ? { payload } : {}),
+        ...(meta ? { meta } : {}),
       };
     };
-    actionCreator.type = type;
-
-    return actionCreator;
   }
 
-  const actionCreator: any = (payload: any) => {
-    return payload ? { type, payload } : { type };
-  };
   actionCreator.type = type;
-
   return actionCreator;
 }
