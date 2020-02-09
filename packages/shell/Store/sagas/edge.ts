669993a656da5442fd3dbd65de38e6bd35caf6de
smith <smith@localhost.localdomain>
2020-02-09 15:49:42 -0500

    mutation types moved a new file

--- packages/shell/Store/sagas/edge.ts
+++ packages/shell/Store/sagas/edge.ts
@@ -2,8 +2,9 @@ import Muelle from '@qml/org.muelle.types-1.0';
 import { Action } from '../../../shared/flux/flux';
 import { cancelled, commit, put, take } from '../../../shared/saga/effects';
 import { slideIn, slideOut, slideOutFinished } from '../actions';
+import { NEXT_EDGE, UPDATE_POSITION } from '../mutationTypes';
 import { contextSelect } from '../selectors/contextSelect';
-import { Context, NEXT_EDGE, UPDATE_POSITION } from '../types';
+import { Context } from '../types';
 
 export function* edge(action: Action<Muelle.Types.Edge>) {
   const { positioner, view, configuration }: Context = yield contextSelect;
@@ -14,7 +15,7 @@ export function* edge(action: Action<Muelle.Types.Edge>) {
 
     // prepare for slide in
     yield commit(NEXT_EDGE, action.payload);
-    configuration.edge = action.payload
+    configuration.edge = action.payload;
     configuration.save();
     positioner.update(0);
     view.setOpacity(0.01);
