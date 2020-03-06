5aa95ac346c025a86052a1dd65d19bd8345037c8
smith <smith@localhost.localdomain>
2020-03-06 00:41:01 -0500

    add Sequence chainable helper

---
+++ packages/shared/sequence.ts
@@ -0,0 +1,40 @@
+export function from<T>(iterable?: T[]) {
+  return new Seq<T>(Array.from(iterable || []));
+}
+
+export default { from };
+
+import * as F from './functional';
+
+class Seq<T> {
+  iterable: T[];
+
+  constructor(iterable: T[]) {
+    this.iterable = iterable;
+  }
+
+  map<
+    Fn extends (item: T, index: number, array: T[]) => any,
+    U = Seq<ReturnType<Fn>>
+  >(fn: Fn): Seq<U> {
+    return (this.iterable = F.map(this.iterable, fn)), <any>this;
+  }
+
+  concat<U>(iterable: U): Seq<T | U> {
+    return this.iterable.concat(<any>iterable), this;
+  }
+
+  filter<Fn extends (item: T, index: number, array: T[]) => any>(
+    fn: Fn
+  ): Seq<T> {
+    return (this.iterable = F.filter(this.iterable, fn)), this;
+  }
+
+  each<Fn extends (item: T, index: number, array: T[]) => any>(fn: Fn): Seq<T> {
+    return F.each(this.iterable, fn), this;
+  }
+
+  get value(): T[] {
+    return this.iterable;
+  }
+}
